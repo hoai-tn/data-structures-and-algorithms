2f449dd750faa62bc4b7d7cd7a4895c3377632dc
hoai tran <tranngochoai02051999@gmail.com>
2023-10-27 13:50:42 +0700

    Add hashtable

---
+++ hash-table/index.js
@@ -0,0 +1,61 @@
+/*
+You'll commonly use a Hash Table because of its fast search, insertion, and delete operations:
+
+*/
+
+class HashTable {
+    constructor() {
+        this.table = new Array(127)
+        this.size = 0
+    }
+
+    _hash(key) {
+        let hash = 0
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i)
+        }
+        return hash % this.table.length
+    }
+
+    set(key, value) {
+        const index = this._hash(key)
+        this.table[index] = [key, value]
+        this.size++
+    }
+
+    get(key) {
+        const target = this._hash(key)
+        return this.table[target]
+    }
+
+    remove(key) {
+        const index = this._hash(key)
+        if (this.table[index] && this.table[index].length) {
+            this.table[index] = []
+            this.size--
+            return true
+        }
+        return false
+    }
+    // remove(key) {
+    //     const index = this._hash(key)
+
+    //     if (this.table[index] && this.table[index].length) {
+    //         this.table[index] = []
+    //         this.size--
+    //         return true
+    //     } else {
+    //         return false
+    //     }
+    // }
+}
+
+const ht = new HashTable()
+ht.set('Canada', 300)
+ht.set('France', 100)
+ht.set('Spain', 110)
+
+console.log(ht.table)
+console.log(ht.remove("Frances"))
+console.log(ht.table)
+
--- linked-lists/index.js
+++ linked-lists/index.js
@@ -15,131 +15,164 @@ Bởi vì trong danh sách liên kết cũng cần có một con trỏ để lư
 */
 
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null; // This points to the next node
-  }
+    constructor(value) {
+        this.value = value
+        this.next = null // This points to the next node
+    }
 }
 class LinkedList {
-  constructor(head = null) {
-    this.head = head;
-  }
-  add(el) {
-    if (typeof el === "number") {
-      const node = new Node(el);
-      let current = this.head;
+    constructor(head = null) {
+        this.head = head
+    }
+    add(el) {
+        if (typeof el === 'number') {
+            const node = new Node(el)
+            let current = this.head
 
-      if (this.head === null) this.head = node;
-      else {
+            if (this.head === null) this.head = node
+            else {
+                while (current.next) {
+                    current = current.next
+                }
+                current.next = node
+            }
+        }
+    }
+    size() {
+        let count = 0
+        let node = this.head
+        while (node) {
+            count++
+            node = node.next
+        }
+        return count
+    }
+    getLast() {
+        let node = this.head
+        while (node) {
+            node = node.next
+            if (node.next === null) return node.value
+        }
+    }
+    getFirst() {
+        return this.head.value
+    }
+    insertAt(el, index) {
+        let count = 0
+        let current = this.head
         while (current.next) {
-          current = current.next;
+            if (count === index) {
+                const node = new Node(el)
+                node.next = current.next
+                current.next = node
+                return
+            }
+            current = current.next
+            count++
         }
-        current.next = node;
-      }
     }
-  }
-  size() {
-    let count = 0;
-    let node = this.head;
-    while (node) {
-      count++;
-      node = node.next;
-    }
-    return count;
-  }
-  getLast() {
-    let node = this.head;
-    while (node) {
-      node = node.next;
-      if (node.next === null) return node.value;
-    }
-  }
-  getFirst() {
-    return this.head.value;
-  }
-  insertAt(el, index) {
-    let count = 0;
-    let current = this.head;
-    while (current.next) {
-      if (count === index) {
-        const node = new Node(el);
-        node.next = current.next;
-        current.next = node;
-        return;
-      }
-      current = current.next;
-      count++;
-    }
-  }
-  removeFrom(index) {
-    if (index < 0) return console.log("Please Enter a valid index");
-    else {
-      var curr,
-        prev,
-        it = 0;
-      curr = this.head;
-      prev = curr;
+    removeFrom(index) {
+        if (index < 0) return console.log('Please Enter a valid index')
+        else {
+            let curr,
+                prev,
+                it = 0
+            curr = this.head
+            prev = curr
 
-      // deleting first element
-      if (index === 0) {
-        this.head = curr.next;
-      } else {
-        // iterate over the list to the
-        // position to remove an element
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr.next;
+            // deleting first element
+            if (index === 0) {
+                this.head = curr.next
+            } else {
+                // iterate over the list to the
+                // position to remove an element
+                while (it < index) {
+                    it++
+                    prev = curr
+                    curr = curr.next
+                }
+
+                // remove the element
+                prev.next = curr.next
+            }
+
+            // return the remove element
+            return curr.value
         }
-
-        // remove the element
-        prev.next = curr.next;
-      }
-
-      // return the remove element
-      return curr.value;
     }
-  }
+    getMiddle() {
+        let high = this.head
+        let low = this.head
+        while (high && high.next) {
+            high = high.next.next
+            low = low.next
+        }
+        return low.value
+    }
 }
+//Reverse a Linked List
+function reverse(node) {
+    let prev = null
+    let next = null
+    let current = node
+    while (current) {
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+    return prev
+}
+//Rotate a Linked List
+
+function rotate () {
+    
+}
+
+
+
 
 // Function to swap the nodes
 function swap(ptr1, ptr2) {
-  let tmp = ptr2.value;
-  ptr2.value = ptr1.value;
-  ptr1.value = tmp;
+    let tmp = ptr2.value
+    ptr2.value = ptr1.value
+    ptr1.value = tmp
 }
-
 // Function to sort the list
 function bubbleSort(head) {
-  let swapped;
-  if (head == null) return;
-  do {
-    swapped = false;
-    let current = head;
-    while (current.next != null) {
-      if (current.value > current.next.value) {
-        swap(current, current.next);
-        swapped = true;
-      }
-      current = current.next;
-    }
-  } while (swapped);
+    let swapped
+    if (head == null) return
+    do {
+        swapped = false
+        let current = head
+        while (current.next != null) {
+            if (current.value > current.next.value) {
+                swap(current, current.next)
+                swapped = true
+            }
+            current = current.next
+        }
+    } while (swapped)
 }
 
-const node1 = new Node(3);
-const node2 = new Node(2);
-const node3 = new Node(1);
+const node1 = new Node(3)
+const node2 = new Node(2)
+const node3 = new Node(1)
 
-node1.next = node2;
-node2.next = node3;
+node1.next = node2
+node2.next = node3
 
-let list = new LinkedList(node1);
-list.add(10);
-list.add(22);
-console.log(JSON.stringify(list));
-list.insertAt(4, 2);
+let list = new LinkedList(node1)
+list.add(10)
+list.add(22)
+list.add(32)
+list.insertAt(4, 2)
 // console.log(list.removeFrom(3));
-bubbleSort(list.head);
-console.log(JSON.stringify(list));
+bubbleSort(list.head)
+console.log(JSON.stringify(list))
+
+console.log('revert')
+list.head = reverse(list.head)
+console.log(JSON.stringify(list))
 
 // console.log(list.getFirst())
